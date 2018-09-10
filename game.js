0451a9ec998b8770565855fd7edd7fa2d0f6cee7
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-09-10 09:47:47 +0200

    Added eventlistener to the star button, and the start new game function is called

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 game = {
     init: function () {
         this.createGameSizeSelector();
+        this.addingEventListeners();
     },
 
     createGameSizeSelector: function () {
         let navbar = document.getElementById("navbar");
         let sizeSelector = document.createElement("select");
+        sizeSelector.id = "game-size-select";
         navbar.appendChild(sizeSelector);
 
         for (let i = 3; i <= 20; i++) {
@@ -14,6 +16,18 @@ game = {
             option.text = i;
             sizeSelector.appendChild(option);
         }
+    },
+
+    addingEventListeners: function () {
+        let newGameButton = document.getElementById("new-game-btn");
+        newGameButton.addEventListener("click", function () {
+            let gameSizeNumber = document.getElementById("game-size-select").value;
+            game.startNewGame(gameSizeNumber);
+        })
+    },
+
+    startNewGame: function (gameSize) {
+        console.log(`New game started with ${gameSize} pairs`);
     }
 };
 
